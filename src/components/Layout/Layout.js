3bb41bd64cb4a7ef274f65a74584453cc111591a
Shane Rocha <67454127+serocha@users.noreply.github.com>
2020-09-25 11:52:59 -0600

    Initial commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+import Aux from '../../hoc/auxiliary';
+import classes from './Layout.module.css';
+
+
+const layout = ( props ) => (
+	<Fragment>
+		<div>Toolbar, SideDrawer, Backdrop</div>
+		<main className={classes.Content}>
+			{props.children}
+		</main>
+	</Fragment>
+);
+
+export default layout;
