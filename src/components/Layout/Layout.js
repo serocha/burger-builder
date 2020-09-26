705761d731fbee70b6a71fe7c2c3bf8a4be7b2bf
Shane Rocha <67454127+serocha@users.noreply.github.com>
2020-09-26 13:25:59 -0600

    completed ui

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 
-import Aux from '../../hoc/auxiliary';
 import classes from './Layout.module.css';
 
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-	<Fragment>
-		<div>Toolbar, SideDrawer, Backdrop</div>
-		<main className={classes.Content}>
-			{props.children}
-		</main>
-	</Fragment>
-);
+class Layout extends Component {
+	state = {
+		showSideDrawer: false
+	}
 
-export default layout;
+	sideDrawerClosedHandler = () => {
+		this.setState({showSideDrawer: false});
+	}
+
+	sideDrawerToggleHandler = () => {
+		this.setState( (prevState) => {
+			return {showSideDrawer: !this.state.showSideDrawer}
+		})
+	}
+
+	render () {
+		return (
+			<Fragment>
+				<Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+				<SideDrawer
+					open={this.state.showSideDrawer} 
+					closed={this.sideDrawerClosedHandler} />
+				<main className={classes.Content}>
+					{this.props.children}
+				</main>
+			</Fragment>
+		);
+	}
+} 
+
+export default Layout;
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+
+import classes from './SideDrawer.module.css';
+
+const sideDrawer = (props) => {
+	let attachedClasses = [classes.SideDrawer, classes.Close];
+	if (props.open) {
+		attachedClasses = [classes.SideDrawer, classes.Open];
+	}
+	return(
+		<Fragment>
+			<Backdrop show={props.open} clicked={props.closed} />
+			<div className={attachedClasses.join(' ')}>
+				<div className={classes.Logo}>
+					<Logo />
+				</div>
+				<nav>
+					<NavigationItems />
+				</nav>
+			</div>
+		</Fragment>
+	);
+}
+
+export default sideDrawer;
